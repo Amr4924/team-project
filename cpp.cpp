0a6cfc2fca770a20e21147368b22ee77582ba416
Amr (SA3DWY) <amr.saad.c25@svnu.edu.eg>
2025-07-05 22:34:59 +0300

    upload all file

---
+++ cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostreeam>
+
+using namespace std;
+
+int main()
+{
+    // Print the result of the factorial function for 5
+    cout << factorial(5);
+
+    return 0; // End of the program
+}
